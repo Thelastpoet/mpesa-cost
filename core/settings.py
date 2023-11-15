2ac77d5a242e7e7678d200d7d9d1943c93bd402b
thelastpoet <emcie4@gmail.com>
2023-11-15 07:39:03 +0300

    Styling

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
     'django_bootstrap5',
     'mpesacal',    
