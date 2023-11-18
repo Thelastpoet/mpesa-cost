5e1d4a3b995b0723a4a67a506e60e231d223b2bd
thelastpoet <emcie4@gmail.com>
2023-11-18 11:09:19 +0300

    Added History

--- mpesacal/urls.py
+++ mpesacal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
-from .views import calculate_fee
+from .views import CalculateFeeView, TransactionHistoryView, BulkDeleteTransactionsView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='https://nasonga.com'), name='home'),
-    path('calculate-fee/', calculate_fee, name='calculate_fee'),
+    path('calculate-fee/', CalculateFeeView.as_view(), name='calculate_fee'),
+    path('transaction-history/', TransactionHistoryView.as_view(), name='transaction_history'),
+    path('transactions/bulk_delete/', BulkDeleteTransactionsView.as_view(), name='bulk_delete_transactions'),
 ]
--- mpesacal/views.py
+++ mpesacal/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 from .models import Transaction, MpesaFee
 from .forms import TransactionForm
 
@@ -13,26 +17,39 @@ def calculate_mpesa_fee(amount, transaction_type):
             return fee
     return 0
 
-def calculate_fee(request):
-    transaction_type = None  
-    transaction_type_descriptions = {
-        'registered_users': 'Send',
-        'agent_withdrawal': 'Withdraw',
-        'unregistered_users': 'Send',
-        'other_network_users': 'Send',
-        'atm_withdrawal': 'Withdraw',
-    }
-    fee = None  
-    amount = None
-    if request.method == 'POST':
+class CalculateFeeView(View):
+    def get(self, request, *args, **kwargs):
+        form = TransactionForm()
+        return render(request, 'calculate_fee.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction_type = form.cleaned_data['transaction_type']
             amount = form.cleaned_data['amount']
             fee = calculate_mpesa_fee(amount, transaction_type)
             user = request.user if request.user.is_authenticated else None
-            Transaction.objects.create(user=user, amount=amount, fee=fee, transaction_type=transaction_type)            
-    else:
-        form = TransactionForm()
-    transaction_type_description = transaction_type_descriptions[transaction_type] if transaction_type else None
-    return render(request, 'mpesacal/calculate_fee.html', {'form': form, 'fee': fee, 'transaction_type': transaction_type_description, 'amount': amount})
+            Transaction.objects.create(user=user, amount=amount, fee=fee, transaction_type=transaction_type)
+            return render(request, 'calculate_fee.html', {'form': form, 'fee': fee, 'transaction_type': transaction_type, 'amount': amount})
+        return render(request, 'calculate_fee.html', {'form': form})
+
+class TransactionHistoryView(View):
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return render(request, 'account/login.html')
+        user_transactions = Transaction.objects.filter(user=request.user).order_by('-created_at')
+        paginator = Paginator(user_transactions, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'transaction_history.html', {'page_obj': page_obj})
+
+class TransactionDeleteView(DeleteView):
+    model = Transaction
+    success_url = reverse_lazy('transaction_history')
+    template_name = 'transaction_confirm_delete.html'
+
+class BulkDeleteTransactionsView(View):
+    def post(self, request, *args, **kwargs):
+        transaction_ids = request.POST.getlist('transaction_ids')
+        Transaction.objects.filter(id__in=transaction_ids, user=request.user).delete()
+        return redirect('transaction_history')
